92089f6bef6b1bd31538519e1f53700775f2e8b5
Marco <marcodamommio.00@gmail.com>
2023-03-09 12:36:18 +0100

    Flores-101 download and extraction script

---
+++ data/scripts/flores-101.sh
@@ -0,0 +1,8 @@
+# run this script from the path_to/translated-llms-for-mt directory
+# as $source data/scripts/flores-101.sh
+# here's a link with additional resources: https://github.com/facebookresearch/flores
+EXTERNAL_PATH=./data/external
+wget -P $EXTERNAL_PATH --trust-server-names https://tinyurl.com/flores200dataset
+tar -xf $EXTERNAL_PATH/flores200_dataset.tar.gz -C $EXTERNAL_PATH
+rm $EXTERNAL_PATH/flores200_dataset.tar.gz
+echo flores-101 dataset in $EXTERNAL_PATH
