50dd770b0790ca8a33e945ff8c104142ee1f2ba5
Mahdi Molaei <97316970+m-molaei@users.noreply.github.com>
2023-03-14 19:12:45 +0330

    Add files via upload

--- scripts/BLEU_score.py
+++ scripts/BLEU_score.py
@@ -7,22 +7,19 @@ from nltk.tokenize import word_tokenize
 import os
 
 
-class Evaluator():
+class Evaluator:
 
-    def __init__(
-            self,
-            source_lang,
-            target_lang
-    ):
-        self.source_lang = source_lang
-        self.target_lang = target_lang
+    def calculate_bleu_score(self, df_prediction):
+        df_prediction['BLEU'] = 0
+        for i, r in df_prediction.iterrows():
+            bleu_score = sentence_bleu([word_tokenize(r['target'])], word_tokenize(r['translation']))
+            df_prediction.at[i, 'BLEU'] = bleu_score
 
-    def calculate_bleu_score(self, dataframe, save_path='/data/'):
+        return df_prediction
+
+    def bleu_score_from_dataframe(self, dataframe, save_path='/data/'):
         df_prediction = dataframe.copy()
-        df_prediction['BLEU'] = 0
-        for i, r in df_prediction.iterrows():
-            bleu_score = sentence_bleu([word_tokenize(r['target'])], word_tokenize(r['translation']))
-            df_prediction.at[i, 'BLEU'] = bleu_score
+        df_prediction = self.calculate_bleu_score(df_prediction)
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
@@ -31,12 +28,9 @@ class Evaluator():
         df_prediction.to_csv(save_path, sep=',')
         return df_prediction
 
-    def calculate_bleu_score(self, prediction_file_path, sep=',', encoding='utf-8', save_path='/data/'):
+    def bleu_score_from_file_path(self, prediction_file_path, sep=',', encoding='utf-8', save_path='/data/'):
         df_prediction = pd.read_csv(prediction_file_path, sep=sep, encoding=encoding)
-        df_prediction['BLEU'] = 0
-        for i, r in df_prediction.iterrows():
-            bleu_score = sentence_bleu([word_tokenize(r['target'])], word_tokenize(r['translation']))
-            df_prediction.at[i, 'BLEU'] = bleu_score
+        df_prediction = self.calculate_bleu_score(df_prediction)
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
@@ -44,6 +38,17 @@ class Evaluator():
         save_path += 'df_prediction_with_BLEU'
         df_prediction.to_csv(save_path, sep=',')
         return df_prediction
+
+    def calculate_corpus_bleu(self, df_translation):
+        list_of_references = []
+        for sentence in df_translation['target'].values:
+            list_of_references.append([word_tokenize(sentence)])
+
+        hypotheses = []
+        for sentence in df_translation['translation'].values:
+            hypotheses.append(word_tokenize(sentence))
+
+        return corpus_bleu(list_of_references, hypotheses)
 
     def calculate_mean_bleu(self, df_prediction):
         mean_bleu = df_prediction.loc[:, 'BLEU'].mean()
