3bf4ac48a5023e40e1461471d68f0f917469396b
Mahdi Molaei <97316970+m-molaei@users.noreply.github.com>
2023-03-14 11:23:06 +0330

    Add files via upload

--- scripts/BLEU score.py
+++ scripts/BLEU score.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import nltk
+
 nltk.download('punkt')
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 from nltk.tokenize import word_tokenize
+import os
 
 
 class Translator():
@@ -13,33 +15,36 @@ class Translator():
             target_lang
     ):
         self.source_lang = source_lang
-
         self.target_lang = target_lang
 
-    def calculate_bleu_score(self, dataframe, save_path = '/data/'):
+    def calculate_bleu_score(self, dataframe, save_path='/data/'):
         df_prediction = dataframe.copy()
         df_prediction['BLEU'] = 0
         for i, r in df_prediction.iterrows():
             bleu_score = sentence_bleu([word_tokenize(r['target'])], word_tokenize(r['translation']))
             df_prediction.at[i, 'BLEU'] = bleu_score
 
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
         save_path += 'df_prediction_with_BLEU'
-        df_prediction.to_csv(save_path , sep = ',')
+        df_prediction.to_csv(save_path, sep=',')
         return df_prediction
 
-    def calculate_bleu_score(self, prediction_file_path, sep = ',' ,encoding = 'utf-8', save_path = '/data/'):
-        df_prediction = pd.read_csv(prediction_file_path, sep = sep, encoding = encoding)
+    def calculate_bleu_score(self, prediction_file_path, sep=',', encoding='utf-8', save_path='/data/'):
+        df_prediction = pd.read_csv(prediction_file_path, sep=sep, encoding=encoding)
         df_prediction['BLEU'] = 0
         for i, r in df_prediction.iterrows():
             bleu_score = sentence_bleu([word_tokenize(r['target'])], word_tokenize(r['translation']))
             df_prediction.at[i, 'BLEU'] = bleu_score
 
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
         save_path += 'df_prediction_with_BLEU'
-        df_prediction.to_csv(save_path , sep = ',')
+        df_prediction.to_csv(save_path, sep=',')
         return df_prediction
 
     def calculate_mean_bleu(self, df_prediction):
         mean_bleu = df_prediction.loc[:, 'BLEU'].mean()
-        print(f'The mean BLEU score of all translated text: {mean_bleu}')
         return mean_bleu
-
