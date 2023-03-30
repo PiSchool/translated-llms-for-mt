3cf4d9879b60ddb1bad50f3b307397945cfe1373
Mahdi <molaei1996@gmail.com>
2023-03-30 16:17:55 +0430

    Refactoring the code by: - Added BLEU2, BLEU3, and BLEU4 scores for both sentence and corpus-level BLEU calculation - Optimized calculation of COMET sentence score - Optimized calculation of COMET system score - Solved issues with running the COMET score

--- mt2magic/evaluator.py
+++ mt2magic/evaluator.py
@@ -19,41 +19,50 @@ class Evaluator:
     def __init__(self, model_name='Unbabel/wmt22-comet-da'):
 
         self.COMET_model_path = download_model(model_name, saving_directory='./models/')
+        self.COMET_sytem_score = 0
 
-    def calculate_sentence_bleu(self, df_evaluation):
+    def calculate_sentence_bleu(self, dataframe):
         """
-            Calculating the sentence BLEU score for each translation.
+        Calculating the sentence BLEU score for each translation.
         """
-        df_evaluation['BLEU'] = 0
+        dataframe['BLEU2'] = 0
+        dataframe['BLEU3'] = 0
+        dataframe['BLEU4'] = 0
         smoothie = SmoothingFunction().method4
-        for i, r in df_evaluation.iterrows():
-            bleu_score = sentence_bleu([word_tokenize(str(r['target']))], word_tokenize(str(r['translation']))
-                                       , smoothing_function=smoothie)
-            df_evaluation.at[i, 'BLEU'] = bleu_score
+        weights = [
+            (1./2., 1./2.),
+            (1./3., 1./3., 1./3.),
+            (1./4., 1./4., 1./4., 1./4.)
+            ]        
+        for i, r in dataframe.iterrows():   
+            bleu_scores = sentence_bleu([word_tokenize(str(r['target']))], word_tokenize(str(r['translation']))
+                                       , weights, smoothing_function=smoothie)
+            
+            dataframe.at[i, 'BLEU2'] = bleu_scores[0]
+            dataframe.at[i, 'BLEU3'] = bleu_scores[1]
+            dataframe.at[i, 'BLEU4'] = bleu_scores[2]
 
-        return df_evaluation
+        return dataframe
 
-    def calculate_sentence_chrf(self, df_evaluation):
+    def calculate_sentence_chrf(self, dataframe):
         """
-            Calculating the sentence chrf score for each translation.
+        Calculating the sentence chrf score for each translation.
         """
-        df_evaluation['chrf'] = 0
-        for i, r in df_evaluation.iterrows():
+        dataframe['chrf'] = 0
+        for i, r in dataframe.iterrows():
             chrf_score = sentence_chrf((str(r['target'])), str(r['translation']))
-            df_evaluation.at[i, 'chrf'] = chrf_score
+            dataframe.at[i, 'chrf'] = chrf_score
 
-        return df_evaluation
+        return dataframe
 
-    def calculate_COMET(self, df_evaluation, batch_size=8, gpu_numbers=1):
+    def calculate_COMET(self, dataframe, batch_size=16, gpu_numbers=1):
         """
-            Calculating the COMET score for each translation.
-            model_name (:obj:`str`): Model name of COMET library from below link:
-                1. https://huggingface.co/Unbabel
-                The default value is 'Unbabel/wmt22-comet-da' which is built on top of XLM-R
-                and has been trained on direct assessments from WMT17 to WMT20 and provides scores ranging from 0 to 1
-                , where 1 represents a perfect translation.
-                batch_size (:obj: 'int'): batch_size
-                gpu_numbers (:obj: 'int'): Number of GPUs
+        Calculating the COMET score for each translation and also COMET sytem_score for entire translations.
+        Args
+            batch_size (:obj: 'int'): batch_size
+            gpu_numbers (:obj: 'int'): Number of GPUs
+        Returns
+            dataframe with added COMET score
         """
         if torch.cuda.is_available():
             gpu_numbers = gpu_numbers
@@ -61,125 +70,140 @@ class Evaluator:
             gpu_numbers = 0
 
         model = load_from_checkpoint(self.COMET_model_path)
-        df_evaluation['COMET'] = 0
-        for i, r in df_evaluation.iterrows():
-            data = [
-                {
-                    'src': str(r['source']),
-                    'mt': str(r['translation']),
-                    'ref': str(r['target'])
+        data_list = []
+        for i, r in dataframe.iterrows():
+            data = {
+                'src': str(r['source']),
+                'mt': str(r['translation']),
+                'ref': str(r['target'])
                 }
-            ]
-            model_output = model.predict(data, batch_size=batch_size, gpus=gpu_numbers)
-            df_evaluation.at[i, 'COMET'] = model_output.scores[0]
+            data_list.append(data)
+        
+        model_output = model.predict(data_list, batch_size, gpu_numbers)
+        dataframe['COMET'] = model_output.scores
 
-        return df_evaluation
+        # Add COMET system_score to self.COMET_sytem_score variable 
+        # so when we need COMET system_score, there won't be any need to recalculate it
+        self.COMET_sytem_score = model_output.system_score
+
+        return dataframe
 
     def evaluating_from_dataframe(self, dataframe, save_path='/data/df_result_with_evaluation.csv'
                                   , COMET_model_batch_size=8, COMET_model_gpu_numbers=1):
         """
-                    Evaluating translations from privided csv file path.
-                    Keys and Values:
-                        dataframe (:obj:`pandas dataframe'): Translation dataframe with agreed structure
-                        save_path (:obj: 'str'): path for saving the result dataframe in csv format
+        Evaluating translations from privided csv file path.
+        Args
+            dataframe (:obj:`pandas dataframe'): Translation dataframe with agreed structure
+            save_path (:obj: 'str'): path for saving the result dataframe in csv format
 
-                    Output:
-                        dataframe (:obj: 'pandas dataframe'): The dataframe with 3 evaluation metrics columns (BLEU, chrf, COMET)
+        Returns
+            dataframe (:obj: 'pandas dataframe'): The dataframe with 3 evaluation metrics columns (BLEU, chrf, COMET)
         """
-        df_evaluation = dataframe.copy()
-        df_evaluation = self.calculate_sentence_bleu(df_evaluation)
-        df_evaluation = self.calculate_sentence_chrf(df_evaluation)
-        df_evaluation = self.calculate_COMET(df_evaluation
+        dataframe = self.calculate_sentence_bleu(dataframe)
+        dataframe = self.calculate_sentence_chrf(dataframe)
+        dataframe = self.calculate_COMET(dataframe
                                             , batch_size=COMET_model_batch_size, gpu_numbers=COMET_model_gpu_numbers)
 
-
-        df_evaluation.to_csv(save_path, sep=',')
-        return df_evaluation
+        dataframe.to_csv(save_path, sep=',')
+        return dataframe
 
     def evaluating_from_file_path(self, prediction_file_path, sep=',', encoding='utf-8', save_path='/data/'
                                   , COMET_model_batch_size=8, COMET_model_gpu_numbers=1):
         """
-                    Evaluating translations from privided csv file path.
-                    Keys and Values:
-                        prediction_file_path (:obj:`str'): CSV file path with agreed structure
-                        sep (:obj: 'str'): seperator of csv file
-                        encoding (:obj: 'str'): encoding of csv file
-                        save_path (:obj: 'str'): path for saving the result dataframe in csv format
+        Evaluating translations from privided csv file path.
+        Args
+            prediction_file_path (:obj:`str'): CSV file path with agreed structure
+            sep (:obj: 'str'): seperator of csv file
+            encoding (:obj: 'str'): encoding of csv file
+            save_path (:obj: 'str'): path for saving the result dataframe in csv format
 
-                    Output:
-                        dataframe (:obj: 'pandas dataframe'): The dataframe with 3 evaluation metrics columns (BLEU, chrf, COMET)
+        Returns
+            dataframe (:obj: 'pandas dataframe'): The dataframe with 3 evaluation metrics columns (BLEU, chrf, COMET)
         """
 
-        df_evaluation = pd.read_csv(prediction_file_path, sep=sep, encoding=encoding)
-        df_evaluation = self.calculate_sentence_bleu(df_evaluation)
-        df_evaluation = self.calculate_sentence_chrf(df_evaluation)
-        df_evaluation = self.calculate_COMET(df_evaluation
+        dataframe = pd.read_csv(prediction_file_path, sep=sep, encoding=encoding)
+        dataframe = self.calculate_sentence_bleu(dataframe)
+        dataframe = self.calculate_sentence_chrf(dataframe)
+        dataframe = self.calculate_COMET(dataframe
                                              , batch_size=COMET_model_batch_size, gpu_numbers=COMET_model_gpu_numbers)
 
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
+        dataframe.to_csv(save_path, sep=',')
+        return dataframe
 
-        df_evaluation.to_csv(save_path, sep=',')
-        return df_evaluation
-
-    def calculate_corpus_bleu(self, df_evaluation):
+    def calculate_corpus_bleu(self, dataframe):
         """
-            Calculating the corpus BLEU score over entire translations.
+        Calculating the corpus BLEU score over entire translations.
+        Args
+            dataframe (:obj:`pandas dataframe`):
+        Return
+            dictionary (:obj: `dict`): dictionary of BLEU2, BLEU3, and BLEU4 scores
         """
         list_of_references = []
-        for sentence in df_evaluation['target'].values:
+        for sentence in dataframe['target'].values:
             list_of_references.append([word_tokenize(str(sentence))])
 
         hypotheses = []
-        for sentence in df_evaluation['translation'].values:
+        for sentence in dataframe['translation'].values:
             hypotheses.append(word_tokenize(str(sentence)))
 
+        weights = [
+            (1./2., 1./2.),
+            (1./3., 1./3., 1./3.),
+            (1./4., 1./4., 1./4., 1./4.)
+            ]
         smoothie = SmoothingFunction().method4
-        return corpus_bleu(list_of_references, hypotheses, smoothing_function=smoothie)
+        bleu_corpus_scores = corpus_bleu(list_of_references, hypotheses, weights, smoothing_function=smoothie)
+        return {'BLEU2': bleu_corpus_scores[0], 'BLEU3': bleu_corpus_scores[1], 'BLEU4': bleu_corpus_scores[2]}
 
-    def calculate_mean_bleu(self, df_evaluation):
+    def calculate_mean_bleu(self, dataframe):
         """
             Calculating the mean BLEU score over entire translations.
         """
-        mean_bleu = df_evaluation.loc[:, 'BLEU'].mean()
+        mean_bleu = dataframe.loc[:, 'BLEU'].mean()
         return mean_bleu
 
-    def calculate_corpus_chrf(self, df_evaluation):
+    def calculate_corpus_chrf(self, dataframe):
         """
-            Calculating the corpus chrf score over entire translations.
+        Calculating the corpus chrf score over entire translations.
         """
         list_of_references = []
-        for sentence in df_evaluation['target'].values:
+        for sentence in dataframe['target'].values:
             list_of_references.append([str(sentence)])
 
         hypotheses = []
-        for sentence in df_evaluation['translation'].values:
+        for sentence in dataframe['translation'].values:
             hypotheses.append([str(sentence)])
 
         return corpus_chrf(list_of_references, hypotheses)
 
-    def calculate_mean_chrf(self, df_evaluation):
+    def calculate_mean_chrf(self, dataframe):
         """
-            Calculating the mean chrf score over entire translations.
+        Calculating the mean chrf score over entire translations.
         """
-        mean_bleu = df_evaluation.loc[:, 'chrf'].mean()
+        mean_bleu = dataframe.loc[:, 'chrf'].mean()
         return mean_bleu
 
-    def calculate_system_score_COMET(self, df_evaluation, batch_size=256, gpu_numbers=1):
-        """
-            Calculate system_score (mean) COMET score over entire translations.
-            Keys and Values:
-                df_prediction (:obj:`pandas dataframe'): Dataframe contains source text, reference text ,and translation text
-                model_name (:obj:`str`): Model name of COMET library from below link:
-                1. https://huggingface.co/Unbabel
-                The default value is 'Unbabel/wmt22-comet-da' which is built on top of XLM-R
-                and has been trained on direct assessments from WMT17 to WMT20 and provides scores ranging from 0 to 1
-                , where 1 represents a perfect translation.
-                batch_size (:obj: 'int'): batch_size
-                gpu_numbers (:obj: 'int'): Number of GPUs
+    def get_system_score_COMET(self):
+        if self.COMET_sytem_score == 0:
+            return 'COMET system score has not been computed yet. Call calculate_system_score_COMET() to compute it directly.'
+        else:
+            return self.COMET_sytem_score
 
-            Output:
-                system_score (:obj: 'float'): The mean COMET score over entire translations.
+    def calculate_system_score_COMET(self, dataframe, batch_size=16, gpu_numbers=1):
+        """
+        Calculate system_score (mean) COMET score over entire translations.
+        Args
+            df_prediction (:obj:`pandas dataframe'): Dataframe contains source text, reference text ,and translation text
+            model_name (:obj:`str`): Model name of COMET library from below link:
+            1. https://huggingface.co/Unbabel
+            The default value is 'Unbabel/wmt22-comet-da' which is built on top of XLM-R
+            and has been trained on direct assessments from WMT17 to WMT20 and provides scores ranging from 0 to 1
+            , where 1 represents a perfect translation.
+            batch_size (:obj: 'int'): batch_size
+            gpu_numbers (:obj: 'int'): Number of GPUs
+
+        Returns
+            system_score (:obj: 'float'): The mean COMET score over entire translations.
         """
         if torch.cuda.is_available():
             gpu_numbers = gpu_numbers
@@ -189,7 +213,7 @@ class Evaluator:
         model = load_from_checkpoint(self.COMET_model_path)
 
         data_list = []
-        for i, r in df_evaluation.iterrows():
+        for i, r in dataframe.iterrows():
             data = {
                 'src': str(r['source']),
                 'mt': str(r['translation']),
