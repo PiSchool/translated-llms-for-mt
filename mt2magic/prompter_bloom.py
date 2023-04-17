7c06074b7dadd5356bb1e9241342d79bb6c84d6d
Gianfree <gianfranco.romani@outlook.it>
2023-04-17 12:51:12 +0200

    Documentation for prompts for BLOOM and T5

--- mt2magic/prompter_bloom.py
+++ mt2magic/prompter_bloom.py
@@ -2,18 +2,20 @@ def BLOOM_prompt(type: str, sentence: str, src_lan: str, trg_lan:str):
     """
     Given a sentence to translated from a source language to a target language, 
     build a prompt for a BLOOM model using a strategy defined by the following types:
-        - 'A': 'Italian: Mi piace il gelato.
-                English:'
-        - 'B': 'Translate the following text from Italian to English:Mi piace il gelato'.
-        - 'C': 'Translate the following text from Italian to English:
-                Italian: Mi piace il gelato.
-                English:'
+        - 'A': '{src_lan}: {src_sent}
+                {trg_lan}:'
+        - 'B': 'Translate the following text from {src_lan} to {trg_lan}:{src_sent}'.
+        - 'C': 'Translate the following text from {src_lan} to {trg_lan}:
+                {src_lan}: {src_sent}
+                {trg_lan}:'
     According to out mentor, the first and third methods are better.
     Args:
         type (str) : type of prompting techniques to use. Possible inputs are: 'A', 'B', OR 'C'
         sentence (str) : sentence that we want to translate
         src_lan (str) : source language
         trg_lan (str) : target language
+    Returns:
+        prompt (str) : prompt to be fed to the model
     """
     prompt = ""
     if type == "A":
